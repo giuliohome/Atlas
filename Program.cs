8e3ee6f79e7bb29a4baad1a812a6de6f552e8c31
Giulio <giuliohome@gmail.com>
2016-01-21 15:54:36 +0100

    cohomology extension

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atlas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var gens = Topos.Gen();
+            foreach (Topos topos in gens)
+            {
+
+                double tot_qty = gens.Where(t => t.Equals(topos)).Sum(t => t.qty);
+                Console.WriteLine(
+                        topos.key1 + "," + topos.key2 + "," +
+                        topos.day1.ToShortDateString() + "," + topos.day1.ToShortDateString() + "," +
+                        topos.qty.ToString() + "," + topos.seq.ToString()
+                    );
+            }
+            var map = new Map();
+            var spaceFiltration = gens.Distinct(map);
+            foreach (Topos topos in spaceFiltration)
+            {
+
+                double tot_qty = gens.Where(t => map.Equals(t,topos)).Sum(t => t.qty);
+                string tot_qty_uom = String.Join(",", gens.Select(t => t.qty_uom).Distinct().ToArray() );
+                Console.WriteLine(
+                        topos.key1 + "," + topos.key2 + "," +
+                        topos.day1.ToShortDateString() + "," + topos.day1.ToShortDateString() + 
+                        " => tot:" + tot_qty.ToString() +  " => uom:" + tot_qty_uom.ToString()
+                    );
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ Topos.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atlas
+{
+    public class Topos
+    {
+        public string key1;
+        public string key2;
+        public DateTime day1;
+        public DateTime day2;
+        public double qty;
+        public string qty_uom;
+        public int seq;
+
+        public static IEnumerable<Topos> Gen()
+        {
+            return new Topos[] {
+                 new Topos(){ key1 ="a", key2="b", day1=DateTime.Now.Date.AddDays(-1), day2=DateTime.Now.Date, qty=100, seq=1, qty_uom="MT"  },
+                 new Topos(){ key1 ="c", key2="d", day1=DateTime.Now.Date.AddDays(-1), day2=DateTime.Now.Date, qty=100, seq=1, qty_uom="BBL"  },
+                 new Topos(){ key1 ="a", key2="b", day1=DateTime.Now.Date.AddDays(-2), day2=DateTime.Now.Date, qty=150, seq=2, qty_uom="BBL"  },
+                 new Topos(){ key1 ="a", key2="b", day1=DateTime.Now.Date.AddDays(-1), day2=DateTime.Now.Date, qty=150, seq=2, qty_uom="BBL"  },
+                 new Topos(){ key1 ="c", key2="d", day1=DateTime.Now.Date.AddDays(-1), day2=DateTime.Now.Date, qty=150, seq=2, qty_uom="MT"   }
+            };
+        }
+
+    }
+    
+	public static class Cohomology {
+		public static bool NullEquals(this object x, object y) {
+			if (x != null && y == null) {
+				return false;
+			}
+			if (x == null && y != null) {
+				return false;
+			}
+			if (x == null && y == null) {
+				return true;
+			}
+			return x.Equals(y);
+		}
+		public static int GetHashFiber(this object obj)
+		{
+			if (obj == null) {
+				return 0;
+			}
+			return obj.GetHashCode();
+		}
+	}
+    
+    public class Map : IEqualityComparer<Topos>
+    {
+
+        public bool Equals(Topos x, Topos y)
+        {
+            if (!x.key1.NullEquals(y.key1))
+            {
+                return false;
+            }
+            if (!x.key2.NullEquals(y.key2))
+            {
+                return false;
+            }
+            if (!x.day1.NullEquals(y.day1))
+            {
+                return false;
+            }
+            if (!x.day2.NullEquals(y.day2))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public int GetHashCode(Topos obj)
+        {
+            return obj.key1.GetHashFiber() ^ obj.key2.GetHashFiber() ^
+                    obj.day1.GetHashFiber() ^ obj.day2 .GetHashFiber();
+        }
+    } 
+}
